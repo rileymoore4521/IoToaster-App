386a76a8db7b3429974b7e56b9e15d6fd32e6e73
Riley Moore <rileymoore4521@gmail.com>
2021-10-20 16:49:48 -0400

    Created and set up viewmodel for home page of IoToaster. Got rid of a lot of hard coding.

---
+++ IoToaster App/IoToaster App/ViewModels/IoToasterViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IoToaster_App.ViewModels
+{
+    class IoToasterViewModel : BindableObject
+    {
+        public IoToasterViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+        public ICommand IncreaseCount { get; }
+        int count = 0;
+        string temperatureDisplay = "Temperature 0 F";
+        string timeRemainingDisplay = "Time Remaining 0 seconds";
+        string cookingStateDisplay = "Stop Cooking";
+        Color cookingStateDisplayBackgroundColor = Color.Red;
+        string cookingPresetDisplay = "Select a Cooking Preset!";
+        string selectedCookingPresetName = "Bagel";
+        public string TemperatureDisplay
+        {
+            get => temperatureDisplay;
+            set
+            {
+                if (value == temperatureDisplay)
+                    return;
+
+                temperatureDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+        public string TimeRemainingDisplay
+        {
+            get => timeRemainingDisplay;
+            set
+            {
+                if (value == timeRemainingDisplay)
+                    return;
+
+                timeRemainingDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CookingStateDisplay
+        {
+            get => cookingStateDisplay;
+            set
+            {
+                if (value == cookingStateDisplay)
+                    return;
+                cookingStateDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+        public Color CookingStateDisplayBackgroundColor
+        {
+            get => cookingStateDisplayBackgroundColor;
+            set
+            {
+                if (value == cookingStateDisplayBackgroundColor)
+                    return;
+                cookingStateDisplayBackgroundColor = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CookingPresetDisplay
+        {
+            get => cookingPresetDisplay;
+            set
+            {
+                if (value == cookingPresetDisplay)
+                    return;
+
+                cookingPresetDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+        void OnIncrease()
+        {
+            if (CookingStateDisplay.Equals("Start Cooking"))
+            {
+                count++;
+                TemperatureDisplay = $"Temperature {count} F";
+                TimeRemainingDisplay = $"Time Remaining {count} seconds";
+                CookingStateDisplay = "Stop Cooking";
+                CookingPresetDisplay = $"Cooking: {selectedCookingPresetName} ";
+                CookingStateDisplayBackgroundColor = Color.Red;
+
+                //add in function call to PI to stop toaster
+            }
+            else
+            {
+                count++;
+                CookingStateDisplayBackgroundColor = Color.Green;
+                CookingStateDisplay = "Start Cooking";
+                CookingPresetDisplay = $"Cooking Preset selected:{selectedCookingPresetName}";
+                //add in function call to PI to stop toaster.
+            }
+        }
+    }
+}
--- IoToaster App/IoToaster App/Views/IoToasterHomePage.xaml.cs
+++ IoToaster App/IoToaster App/Views/IoToasterHomePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,59 +15,8 @@ namespace IoToaster_App.Views
         public IoToasterHomePage()
         {
             InitializeComponent();
-            
-            BindingContext = this;
-           
-        }
-        int count = 0;
-        string temperatureDisplay = "Temperature 0 F";
-        string timeRemainingDisplay = "Time Remaining 0 seconds";
-        public string TemperatureDisplay
-        {
-            get => temperatureDisplay;
-            set
-            {
-                if (value == temperatureDisplay)
-                    return;
-
-                temperatureDisplay = value;
-                OnPropertyChanged();
-            }
-        }
-        public string TimeRemainingDisplay
-        {
-            get => timeRemainingDisplay;
-            set
-            {
-                if (value == timeRemainingDisplay)
-                    return;
-
-                timeRemainingDisplay = value;
-                OnPropertyChanged();
-            }
-        }
-        private void StartStopButton_Clicked(object sender, EventArgs e)
-        {
-            if (StartStopButton.Text.Equals("Start Cooking"))
-            {
-                count++;
-                TemperatureDisplay = $"Temperature {count} F";
-                TimeRemainingDisplay = $"Time Remaining {count} seconds";
-                StartStopButton.Text = "Stop Cooking";
-                StartStopButton.BackgroundColor = Color.Red;
-              
-                //add in function call to PI to stop toaster
-            }
-            else
-            {
-                count++;
-                StartStopButton.BackgroundColor = Color.Green;
-                StartStopButton.Text = "Start Cooking";
-               
-                //add in function call to PI to stop toaster.
-            }
-
 
         }
+   
     }
 }
