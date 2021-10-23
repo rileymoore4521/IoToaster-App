a0657a428ad4e1f3f6d7f78d50b74d8c664fbae1
Riley Moore <rileymoore4521@gmail.com>
2021-10-23 16:51:45 -0400

    Implemented PATCH for data section for cooking instructions. When start cooking is selected a timestamp plus time in seconds is updated for instructions column. if stop cooking is selected a time stamp plus stop cooking is updated for instructions column.

--- IoToaster App/IoToaster App/Services/InternetCookingPresetService.cs
+++ IoToaster App/IoToaster App/Services/InternetCookingPresetService.cs
@@ -8,7 +8,7 @@ using Xamarin.Essentials;
 using IoToaster_App.Models;
 
 using Newtonsoft.Json;
-
+using MvvmHelpers;
 
 namespace IoToaster_App.Services
 {
@@ -17,10 +17,12 @@ namespace IoToaster_App.Services
 
     public static class InternetCookingPresetService
     {
+       
 
         static string BaseUrl = "https://zbechhoefer-052e.restdb.io/rest/";
 
         static HttpClient client;
+        
         static InternetCookingPresetService()
         {
             client = new HttpClient
@@ -31,7 +33,8 @@ namespace IoToaster_App.Services
             client.DefaultRequestHeaders.Add("cache-control", "no-cache");
             client.DefaultRequestHeaders.Add("x-apikey", "a0fed473e5a6170c2a06583f0030cf12247f6");
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-          
+
+           
         }
         public static async Task<IEnumerable<CookingPreset>> GetCookingPresets()
         {
@@ -67,6 +70,104 @@ namespace IoToaster_App.Services
             {
 
             }
+
+        }
+        public static async Task EditCookingPreset(string id, CookingPreset cookingPreset)
+        {
+            var json = JsonConvert.SerializeObject(cookingPreset);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync($"cookingpresets/{id}", content);
+            if (!response.IsSuccessStatusCode)
+            {
+
+            }
+
+        }
+        public class DataInfo
+        {
+            public string _id;
+            public string phone_instr;
+            
+        };
+        public static ObservableRangeCollection<DataInfo> Datainfo { get; set; }
+        public static async Task<IEnumerable<DataInfo>> GetDataInfo()
+        {
+            var json = await client.GetStringAsync("data");
+            var datainfo = JsonConvert.DeserializeObject<IEnumerable<DataInfo>>(json);
+            return datainfo;
+        }
+        
+        public static async Task UpdateCookingStatus(CookingPreset cookingPreset, bool stopCooking)
+        {
+            DateTime localDate = DateTime.Now;
+            TimeSpan localTime = localDate.TimeOfDay;
+            localTime = new TimeSpan(localTime.Hours, localTime.Minutes, localTime.Seconds);
+            string updateStr = localTime.ToString();
+            Datainfo = new ObservableRangeCollection<DataInfo>();
+            var dataInfo = await GetDataInfo();
+            Datainfo.AddRange(dataInfo);
+            
+            string dataId = "";
+            int numOfItems = Datainfo.Count;
+            if (Datainfo != null && numOfItems != 0)
+            {
+                dataId = Datainfo[0]._id;
+
+                if (stopCooking == false)
+                {
+                    updateStr = updateStr + $",{cookingPreset.ToastDuration}";
+                    Datainfo[0].phone_instr = updateStr;
+                    var json = JsonConvert.SerializeObject(Datainfo[0]);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var method = new HttpMethod("PATCH");
+                    var request = new HttpRequestMessage(method, BaseUrl + $"data/{dataId}")
+                    {
+                        Content = content
+                    };
+                    var response = new HttpResponseMessage();
+
+                    try
+                    {
+                        response = await client.SendAsync(request);
+                    }
+                    catch (TaskCanceledException e)
+                    {
+
+                    }
+                    if (!response.IsSuccessStatusCode)
+                    {
+
+                    }
+                }
+                else if (stopCooking == true)
+                {
+                    updateStr = updateStr + ",Stop Cooking";
+                    Datainfo[0].phone_instr = updateStr;
+                    var json = JsonConvert.SerializeObject(Datainfo[0]);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var method = new HttpMethod("PATCH");
+                    var request = new HttpRequestMessage(method, BaseUrl + $"data/{dataId}")
+                    {
+                        Content = content
+                    };
+                    var response = new HttpResponseMessage();
+
+                    try
+                    {
+                        response = await client.SendAsync(request);
+                    }
+                    catch (TaskCanceledException e)
+                    {
+
+                    }
+                    if (!response.IsSuccessStatusCode)
+                    {
+
+                    }
+
+                }
+            }
+
 
         }
 
--- IoToaster App/IoToaster App/ViewModels/IoToasterViewModel.cs
+++ IoToaster App/IoToaster App/ViewModels/IoToasterViewModel.cs
@@ -26,29 +26,79 @@ namespace IoToaster_App.ViewModels
 
         public AsyncCommand<CookingPreset> RemoveCommand { get; }
 
+        public AsyncCommand<CookingPreset> EditCommand { get; }
         public AsyncCommand<CookingPreset> StartCookingCommand { get; }
         public IoToasterViewModel()
         {
             Title = "IoToaster App";
 
             CookingPresets = new ObservableRangeCollection<CookingPreset>();
-        
+
 
 
             RefreshCommand = new AsyncCommand(Refresh);
             StartCookingCommand = new AsyncCommand<CookingPreset>(StartCooking);
             AddCommand = new AsyncCommand(Add);
             RemoveCommand = new AsyncCommand<CookingPreset>(Remove);
+            EditCommand = new AsyncCommand<CookingPreset>(Edit);
+            
+        }
+        private Color cookingButtonBgColor = Color.Green;
+        public Color CookingButtonBgColor
+        {
+            get => cookingButtonBgColor;
+            set
+            {
+                if (value == cookingButtonBgColor)
+                    return;
+
+                cookingButtonBgColor = value;
+                OnPropertyChanged();
+
+            }
+        }
+        private string cookingButtonText = "Start Cooking";
+        public string CookingButtonText
+        {
+            get => cookingButtonText;
+            set
+            {
+                if (value == cookingButtonText)
+                    return;
+
+                cookingButtonText = value;
+                OnPropertyChanged();
+
+            }
         }
 
+
+
+        bool isCooking = false;
+        
         async Task StartCooking(CookingPreset cookingPreset)
         {
-            if (cookingPreset == null)
-                return;
             
-            await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Started Cooking", cookingPreset.Name, "OK");
+            if(cookingPreset != null && !isCooking)
+            {
+                isCooking = true;
+                CookingButtonBgColor = Color.Red;
+                CookingButtonText = "Stop Cooking";
+                await InternetCookingPresetService.UpdateCookingStatus(cookingPreset, false);
+                await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Started Cooking", cookingPreset.Name, "OK");
+            }else if (cookingPreset != null && isCooking)
+            {
+                isCooking = false;
+                CookingButtonBgColor = Color.Green;
+                CookingButtonText = "Start Cooking";
+                await InternetCookingPresetService.UpdateCookingStatus(cookingPreset, true);
+                await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Stopped Cooking", cookingPreset.Name, "OK");
+            }
+            
+            
 
         }
+       
         CookingPreset selectedCookingPreset;
 
         public CookingPreset SelectedCookingPreset
@@ -78,6 +128,17 @@ namespace IoToaster_App.ViewModels
         async Task Remove(CookingPreset cookingPreset)
         {
             await InternetCookingPresetService.RemoveCookingPreset(cookingPreset._id);
+            await Refresh();
+        }
+        async Task Edit(CookingPreset cookingPreset)
+        {
+            var name = await App.Current.MainPage.DisplayPromptAsync("Name", "The name the preset will be saved under");
+            var toastDuration = await App.Current.MainPage.DisplayPromptAsync("Toast Duration", "The duration the item will be toasted");
+            var temperature = await App.Current.MainPage.DisplayPromptAsync("Temperature", "The Temperature to cook the item at");
+            cookingPreset.Name = name;
+            cookingPreset.ToastDuration = Convert.ToInt32(toastDuration);
+            cookingPreset.Temperature = Convert.ToInt32(temperature);
+            await InternetCookingPresetService.EditCookingPreset(cookingPreset._id,cookingPreset);
             await Refresh();
         }
         async Task Refresh()
